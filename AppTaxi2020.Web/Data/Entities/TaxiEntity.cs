e3cfe932ecc18be6fceb36473c1666187cba4996
Emilio <barrera_emilio@yahoo.es>
2020-02-07 22:02:09 +0000

    Taxi Entity

---
+++ AppTaxi2020.Web/Data/Entities/TaxiEntity.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AppTaxi2020.Web.Data.Entities
+{
+    public class TaxiEntity
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(6, MinimumLength = 6, ErrorMessage = "The {0} field must have {1} character.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public string Plaque { get; set; }
+    }
+}
