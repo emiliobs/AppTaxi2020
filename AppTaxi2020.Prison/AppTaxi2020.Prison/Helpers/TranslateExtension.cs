d0e675ad6f4b14907252f8504dea2e84f8e22de5
Emilio Barrea <barrera_emilio@yahoo.es>
2020-05-30 13:38:01 +0100

    11. Now to translate literals directly in the XAML add the class TranslateExtension in folder Helpers - the UI

---
+++ AppTaxi2020.Prison/AppTaxi2020.Prison/Helpers/TranslateExtension.cs
@@ -0,0 +1,55 @@
+﻿using AppTaxi2020.Prison.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppTaxi2020.Prison.Helpers
+{
+    [ContentProperty("Text")]
+    public class TranslateExtension : IMarkupExtension
+    {
+        private readonly CultureInfo ci;
+        private const string ResourceId = "AppTaxi2020.Prison.Resources.Resource";
+        private static readonly Lazy<ResourceManager> ResMgr =
+            new Lazy<ResourceManager>(() => new ResourceManager(
+                ResourceId,
+                typeof(TranslateExtension).GetTypeInfo().Assembly));
+
+        public TranslateExtension()
+        {
+            ci = DependencyService.Get<ILocalize>().GetCurrentCultureInfo();
+        }
+
+        public string Text { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Text == null)
+            {
+                return "";
+            }
+
+            var translation = ResMgr.Value.GetString(Text, ci);
+
+            if (translation == null)
+            {
+#if DEBUG
+                throw new ArgumentException(
+                    string.Format(
+                        "Key '{0}' was not found in resources '{1}' for culture '{2}'.",
+                        Text, ResourceId, ci.Name), "Text");
+#else
+        	translation = Text; // returns the key, which GETS DISPLAYED TO THE USER
+#endif
+            }
+
+            return translation;
+        }
+    }
+
+}
