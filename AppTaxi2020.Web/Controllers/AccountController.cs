221a9aba12ec44d69bcaa1464f078436dab53725
Emilio Barrea <barrera_emilio@yahoo.es>
2020-06-14 15:47:54 +0100

    Fix recover and change password on AccountContronllore MVC

--- AppTaxi2020.Web/Controllers/AccountController.cs
+++ AppTaxi2020.Web/Controllers/AccountController.cs
@@ -41,13 +41,13 @@ namespace AppTaxi2020.Web.Controllers
        
 
         [HttpGet]
-        public IActionResult RecoverPassword()
+        public IActionResult RecoverPasswordMVC()
         {
             return View();
         }
 
         [HttpPost]
-        public async Task<IActionResult> RecoverPassword(RecoverPasswordViewModel model)
+        public async Task<IActionResult> RecoverPasswordMVC(RecoverPasswordViewModel model)
         {
             if (ModelState.IsValid)
             {
@@ -71,7 +71,7 @@ namespace AppTaxi2020.Web.Controllers
 
             return View(model);
         }
-
+       
         [HttpGet]
         public IActionResult ResetPassword(string token)
         {
@@ -166,13 +166,13 @@ namespace AppTaxi2020.Web.Controllers
         }
 
 
-        public IActionResult ChangePassword()
+        public IActionResult ChangePasswordMVC()
         {
             return View();
         }
 
         [HttpPost]
-        public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model)
+        public async Task<IActionResult> ChangePasswordMVC(ChangePasswordViewModel model)
         {
             if (ModelState.IsValid)
             {
